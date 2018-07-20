91532a89f5080bc0c24fa7dfa8d437d04b0bcedb
firepokemon2014 <40245068+firepokemon2014@users.noreply.github.com>
2018-07-20 13:40:16 -0500

    The Test Code for text and time

--- TextTest.py
+++ TextTest.py
@@ -3,8 +3,10 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((640, 480))
-#clock = pygame.time.Clock()
+clock = pygame.time.Clock()
 done = False
+
+startTime = pygame.time.get_ticks()
 
 font = pygame.font.SysFont(None, 30)
 
@@ -19,9 +21,13 @@ while not done:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             done = True
 
+    gameTime = pygame.time.get_ticks() - startTime
+
+    timeNum = font.render(str(gameTime/1000), True, (0, 128, 0))
 
     screen.fill((255, 255, 255))
     screen.blit(score, (500,10))
     screen.blit(time, (500,30))
+    screen.blit(timeNum, (570,30))
     pygame.display.flip()
     #clock.tick(60)
