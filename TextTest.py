fb6f64ac41fe1f4871fda1a30682d0bcff6df96e
firepokemon2014 <40245068+firepokemon2014@users.noreply.github.com>
2018-07-23 14:23:38 -0500

    Start Screen

--- TextTest.py
+++ TextTest.py
@@ -1,66 +1,98 @@
-import pygame
+import sys,os,pygame,random,math
 
 pygame.init()
+pygame.font.init()
 
 pygame.display.set_caption("Testing for Snake Game")
 font = pygame.font.SysFont(None, 30) # Text
-screen = pygame.display.set_mode((640, 480))
+screen_w = 640
+screen_h = 480
+screen = pygame.display.set_mode([screen_w, screen_h])
 clock = pygame.time.Clock() # Time
-done = False
-#screen.fill((255, 255, 255))
+#done = False
+screen.fill((255, 255, 255))
 
 
-#def start():
+def main():
         
-        #startMes = font.render("Snake Arcade", True, (0, 128, 0))
-        #playMes = font.render("Play? Y/N", True, (0, 128, 0))
-        #screen.blit(startMes,(320,240))
-        #screen.blit(playMes,(320+12,240+40))
+        done = False
         
-        #pygame.display.flip()
-        #pygame.display.update()
+        while not done:
+                for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN and event.key == pygame.K_n:
+                                done = True
+                        if event.type == pygame.KEYDOWN and event.key == pygame.K_y:
+                                start()
+                                
+                startMes = font.render("Snake Arcade", True, (0, 128, 0))
+                playMes = font.render("Play? Y/N", True, (0, 128, 0))
+                
+                screen.fill((255, 255, 255))
+                screen.blit(startMes,(320,240))
+                screen.blit(playMes,(320+12,240+40))
+                pygame.display.flip()
+                pygame.display.update()                
+                                
+def start():
         
-        #while not done:
-                #for event in pygame.event.get():
-                #if event.type == pygame.KEYDOWN:
-                        #if event.key == pygame.K_Y:
-                                #main()
-                        #elif event.key == pygame.K_N:
-                                #done = True
-        
-#def main():
-    
-done = False
+        done = False
 
-startTime = pygame.time.get_ticks() # Time
+        startTime = pygame.time.get_ticks() # Time
 
-scoreNum = 0 # score
+        scoreNum = 0 # score
 
-score = font.render("Score: ", True, (0, 128, 0)) # Text and score
-time = font.render("Time: ", True, (0, 128, 0)) # Text and time
-while not done:
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            done = True
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            scoreNum += 10 # score
+        score = font.render("Score: ", True, (0, 128, 0)) # Text and score
+        time = font.render("Time: ", True, (0, 128, 0)) # Text and time
 
-    gameTime = pygame.time.get_ticks() - startTime # Time
+        while not done:
+                for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                                done = True
+                        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                                done = True
+                        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                                scoreNum += 10 # score
+                        if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
+                                endGame()                       
 
-    timeNum = font.render(str(gameTime/1000), True, (0, 128, 0)) # Text and time
+                gameTime = pygame.time.get_ticks() - startTime # Time
 
-    scoreTotal = font.render(str(scoreNum), True, (0, 128, 0)) # Text and score
+                timeNum = font.render(str(gameTime/1000), True, (0, 128, 0)) # Text and time
 
-    screen.fill((255, 255, 255))
-    screen.blit(score, (500,10)) # Text and score
-    screen.blit(time, (500,30)) # Text and time
-    screen.blit(timeNum, (570,30)) # Text and time
-    screen.blit(scoreTotal, (570, 10)) # Text and score
-    pygame.display.flip()
-    pygame.display.update()
-    #clock.tick(60)
+                scoreTotal = font.render(str(scoreNum), True, (0, 128, 0)) # Text and score
+
+                screen.fill((255, 255, 255))
+                screen.blit(score, (500,10)) # Text and score
+                screen.blit(time, (500,30)) # Text and time
+                screen.blit(timeNum, (570,30)) # Text and time
+                screen.blit(scoreTotal, (570, 10)) # Text and score
+                pygame.display.flip()
+                pygame.display.update()
+                #clock.tick(60)
             
-#main()           
+def endGame():
+        
+        done = False
+        
+        endMes = font.render("Game Over", True, (0, 128, 0))
+        conMes = font.render("Play again? Y/N", True, (0, 128, 0))
+                
+                
+        screen.blit(endMes,(320,240))
+        screen.blit(conMes,(320+12,240+40))
+        pygame.display.flip()
+        pygame.display.update()
+                
+        while not done:
+                for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN and event.key == pygame.K_n:
+                                done = True
+                        if event.type == pygame.KEYDOWN and event.key == pygame.K_y:
+                                start()
+                                
+                      
+        
+        
+
+            
+main()           
