168467db14e4a60a777d4b939ba91a58fc2bc5d4
firepokemon2014 <40245068+firepokemon2014@users.noreply.github.com>
2018-07-26 15:40:39 -0500

    heres the random code

--- TextTest.py
+++ TextTest.py
@@ -12,100 +12,124 @@ clock = pygame.time.Clock() # Time
 done = False
 screen.fill((255, 255, 255))
 
+number = random.randrange(1, 8)
 
-def main():
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+                        sys.exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                        done = True
+                        sys.exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                        scoreNum += 10 # score
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
+                        endGame()   
+
+
+                randNum = font.render(str(number), True, (0, 0, 0))
+
+                screen.fill((255, 255, 255))
+                screen.blit(randNum, (200, 200))
+                pygame.display.flip()
+                pygame.display.update()
+
+
+
+#def main():
         
-        done = False
+        #done = False
         
-        startMes = font.render("Snake Arcade", True, (0, 128, 0))
-        playMes = font.render("Play? Y/N", True, (0, 128, 0))
+        #startMes = font.render("Snake Arcade", True, (0, 128, 0))
+        #playMes = font.render("Play? Y/N", True, (0, 128, 0))
                 
         
-        screen.blit(startMes,(320,240))
-        screen.blit(playMes,(320+12,240+40))
-        pygame.display.flip()
-        pygame.display.update()
+        #screen.blit(startMes,(320,240))
+        #screen.blit(playMes,(320+12,240+40))
+        #pygame.display.flip()
+        #pygame.display.update()
                 
-        while not done:
-                for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                                done = True
-                                sys.exit()
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                                done = True
-                                sys.exit()
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_n:
-                                done = True
-                                sys.exit()
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_y:
-                                start()
+        #while not done:
+                #for event in pygame.event.get():
+                        #if event.type == pygame.QUIT:
+                                #done = True
+                                #sys.exit()
+                        #if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                                #done = True
+                                #sys.exit()
+                        #if event.type == pygame.KEYDOWN and event.key == pygame.K_n:
+                                #done = True
+                                #sys.exit()
+                        #if event.type == pygame.KEYDOWN and event.key == pygame.K_y:
+                                #start()
                                 
                              
                                 
-def start():
+#def start():
         
-        done = False
+        #done = False
 
-        startTime = pygame.time.get_ticks() # Time
+        #startTime = pygame.time.get_ticks() # Time
 
-        scoreNum = 0 # score
+        #scoreNum = 0 # score
 
-        score = font.render("Score: ", True, (0, 128, 0)) # Text and score
-        time = font.render("Time: ", True, (0, 128, 0)) # Text and time
+        #score = font.render("Score: ", True, (0, 128, 0)) # Text and score
+        #time = font.render("Time: ", True, (0, 128, 0)) # Text and time
 
-        while not done:
-                for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                                done = True
-                                sys.exit()
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                                done = True
-                                sys.exit()
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                                scoreNum += 10 # score
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
-                                endGame()                       
+        #while not done:
+                #for event in pygame.event.get():
+                        #if event.type == pygame.QUIT:
+                                #done = True
+                                #sys.exit()
+                        #if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                                #done = True
+                                #sys.exit()
+                        #if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                                #scoreNum += 10 # score
+                        #if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
+                                #endGame()                       
 
-                gameTime = pygame.time.get_ticks() - startTime # Time
+                #gameTime = pygame.time.get_ticks() - startTime # Time
 
-                timeNum = font.render(str(gameTime/1000), True, (0, 128, 0)) # Text and time
+                #timeNum = font.render(str(gameTime/1000), True, (0, 128, 0)) # Text and time
 
-                scoreTotal = font.render(str(scoreNum), True, (0, 128, 0)) # Text and score
+                #scoreTotal = font.render(str(scoreNum), True, (0, 128, 0)) # Text and score
 
-                screen.fill((255, 255, 255))
-                screen.blit(score, (500,10)) # Text and score
-                screen.blit(time, (500,30)) # Text and time
-                screen.blit(timeNum, (570,30)) # Text and time
-                screen.blit(scoreTotal, (570, 10)) # Text and score
-                pygame.display.flip()
-                pygame.display.update()
-                #clock.tick(60)
+                #screen.fill((255, 255, 255))
+                #screen.blit(score, (500,10)) # Text and score
+                #screen.blit(time, (500,30)) # Text and time
+                #screen.blit(timeNum, (570,30)) # Text and time
+                #screen.blit(scoreTotal, (570, 10)) # Text and score
+                #pygame.display.flip()
+                #pygame.display.update()
+                ##clock.tick(60)
             
-def endGame():
+#def endGame():
         
-        done = False
+        #done = False
         
-        endMes = font.render("Game Over", True, (0, 128, 0))
-        conMes = font.render("Play again? Y/N", True, (0, 128, 0))
+        #endMes = font.render("Game Over", True, (0, 128, 0))
+        #conMes = font.render("Play again? Y/N", True, (0, 128, 0))
                 
                 
-        screen.blit(endMes,(320,240))
-        screen.blit(conMes,(320+12,240+40))
-        pygame.display.flip()
-        pygame.display.update()
+        #screen.blit(endMes,(320,240))
+        #screen.blit(conMes,(320+12,240+40))
+        #pygame.display.flip()
+        #pygame.display.update()
                 
-        while not done:
-                for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                                done = True
-                                sys.exit()
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                                done = True
-                                sys.exit()
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_n:
-                                done = True
-                                sys.exit()
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_y:
-                                start()
+        #while not done:
+                #for event in pygame.event.get():
+                        #if event.type == pygame.QUIT:
+                                #done = True
+                                #sys.exit()
+                        #if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                                #done = True
+                                #sys.exit()
+                        #if event.type == pygame.KEYDOWN and event.key == pygame.K_n:
+                                #done = True
+                                #sys.exit()
+                        #if event.type == pygame.KEYDOWN and event.key == pygame.K_y:
+                                #start()
                                             
-main()           
+#main()           
